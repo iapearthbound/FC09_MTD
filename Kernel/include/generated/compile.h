dfcbf3455fa77b54a19cb94c10012b3d5494d0d4
Justin D. Maggiolo <earthbound.iap@gmail.com>
2012-05-05 05:58:30 -0400

    initial fc09 mtd commit

---
+++ Kernel/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 4 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 PREEMPT Sat May 5 01:48:52 EDT 2012"
+#define LINUX_COMPILE_TIME "01:48:52"
+#define LINUX_COMPILE_BY "earthbound"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
